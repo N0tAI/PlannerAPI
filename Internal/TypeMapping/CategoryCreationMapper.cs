6c8d06b1344f4c40f2ec55216584e263c97248d0
NotAI <personal@notai.xyz>
2024-09-01 18:13:22 -0400

    Added category creation querying

---
+++ Internal/TypeMapping/CategoryCreationMapper.cs
@@ -0,0 +1,13 @@
+using TaskPlanner.API.Database.Models;
+using TaskPlanner.API.Web;
+
+namespace TaskPlanner.API.Internal.TypeMapping;
+
+public class CategoryCreationMapper : IModelViewMapper<CategoryDbModel, CategoryCreationRequest>
+{
+    public CategoryCreationRequest Map(CategoryDbModel model)
+        => throw new NotImplementedException();
+
+    public CategoryDbModel Map(CategoryCreationRequest view)
+        => new CategoryDbModel { CategoryId = default, Name = view.Name };
+}
---
+++ Querying/Categories/CategoryCreateQuery.cs
@@ -0,0 +1,26 @@
+using System;
+using TaskPlanner.API.Database;
+using TaskPlanner.API.Internal.TypeMapping;
+using TaskPlanner.API.Web;
+
+namespace TaskPlanner.API.Querying.Categories;
+
+public class CategoryCreateQuery : IRepositoryCreateQuery<CategoryCreationRequest>
+{
+    private PlannerDbContext _context;
+    public CategoryCreateQuery(PlannerDbContext context)
+    {
+        _context = context;
+    }
+    public int Execute(IEnumerable<CategoryCreationRequest> requests)
+    {
+        var mapper = new CategoryCreationMapper();
+        var repo = new CategoryRepository(_context);
+
+        int numCreated = 0;
+        foreach(var request in requests)
+            numCreated += repo.Create(mapper.Map(request));
+
+        return numCreated;
+    }
+}
